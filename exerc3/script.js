4a7a2546a4f05aca8666aa9ce12f46597051e4cb
Cardwx <lcb.barbeiro@gmail.com>
2022-06-18 09:40:22 -0300

    construçao lógica sequencia fibo

--- exerc3/script.js
+++ exerc3/script.js
@@ -1,11 +1,11 @@
 
-
+/*
 function calcular(){
     var valor = document.querySelector(".input-text").value
     var resposta = document.querySelector("#resposta")
     var ultimo = 1;var penultimo = 0;var numero = [];
-    var fib = 20;
-    if(fib <= 2){
+   
+    if(valor <= 2){
         numero = valor -  1
     }  else
     for(var count=3 ; count<=fib ; count++){
@@ -13,10 +13,19 @@ function calcular(){
      penultimo = ultimo;
      ultimo = numero;
      
-    }
+    }}*/
 
+const numero = 4
+let n1 = 0, n2 = 1, proximoTermo;
+
+
+console.log("O número informado foi:",numero)
+for (let i = 1; i <= numero; i++) {
+    console.log(n1);
+    proximoTermo = n1 + n2;
+    n1 = n2;
+    n2 = proximoTermo;
+}
+console.log("O número não pertence a sequencia fibonaci")
   
-   //resposta.innerHTML=numero;
- }
-
 
