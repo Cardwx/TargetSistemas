ff22571cd879448a7f9b336fd2f8c0fad0b4aa15
Cardwx <lcb.barbeiro@gmail.com>
2022-06-22 20:36:51 -0300

    exerc 5 feito

---
+++ script.js
@@ -0,0 +1,9 @@
+//cria função de conversão
+function inverterString(str) {
+    var o = '';
+    for (var i = str.length - 1; i >= 0; i--) {
+        o += str[i];
+    }
+    return o;
+}
+console.log(inverterString("Realizando teste de inversão"));
